a881b9b8ee0d45788038bfb0a765ea02da6b79a9
Ilya Staver <Ilya.Staver@thomsonreuters.com>
2021-06-07 22:40:51 +0300

    task-7

--- authorization-service/webpack.config.js
+++ authorization-service/webpack.config.js
@@ -4,16 +4,12 @@ const nodeExternals = require("webpack-node-externals");
 module.exports = {
     entry: slsw.lib.entries,
     target: "node",
-    // Generate sourcemaps for proper error messages
-    // Since 'aws-sdk' is not compatible with webpack,
-    // we exclude all node dependencies
     externals: [nodeExternals()],
     mode: slsw.lib.webpack.isLocal ? "development" : "production",
     performance: {
         // Turn off size warnings for entry points
         hints: false
     },
-    // Run babel on all .js files and skip those in node_modules
     module: {
         rules: [
             {
